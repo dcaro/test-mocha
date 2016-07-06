18c128e103baca77f9d0e774a049ac0159dbcde1
Damien Caro <dcaro@microsoft.com>
2016-07-06 15:38:39 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,39 @@
+var assert = require('assert');
+	fs = require('fs');
+	fileName = '2016-05-19-template.md'
+
+ var tags =[ 'title: ', 'author: ', 'date: ', 'color: ']; 
+
+describe("Check Front Matter", function(){
+ 	var contents = fs.readFileSync(fileName, 'UTF8');
+	it("Verify presence of tags", function () {
+			
+		//frontmatter = /---\s*([\s\S]*?)\s*---/
+		frontmatter = /---\s*((layout:)+)([\s\S]*?)((title:)+)([\s\S]*?)((author:)+)([\s\S]*?)((date:)+)([\s\S]*?)((color: "blue")+)([\s\S]*?)((excerpt:)+)([\s\S]*?)\s*---/
+		assert.ok (frontmatter.test(contents));
+
+	});
+});
+
+describe("Content of the MD file", function(){
+ 	var contents = fs.readFileSync(fileName, 'UTF8');
+	it("Verify customer profile", function () {			
+		pattern = /\s*((## [Cc]ustomer [Pp]rofile ##)+)([\s\S]*?)\s*/
+		assert.ok (pattern.test(contents));
+	});
+	
+	it("Verify conclusion", function () {
+		pattern = /\s*((## [Cc]onclusion ##)+)([\s\S]*?)\s*/
+		assert.ok (pattern.test(contents));
+
+	});
+
+		it("Verify resources", function () {
+		pattern = /\s*((## [Rr]esources ##)+)([\s\S]*?)\s*/
+		assert.ok (pattern.test(contents));
+
+	});
+});
+
+
+
